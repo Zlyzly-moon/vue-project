8b8dd15fd10a7cd011374e8c688e810a07b0a06e
unknown <you@example.com>
2020-01-08 11:17:29 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button class="btn1">测试按钮1</button>
+    <button class="btn2">测试按钮2</button>
+    <ul>
+      <li @click="handleClick(item)" :class="{active: navIndex === item}" v-for="item in 10" :key="item">{{item}}</li>
+    </ul>
   </div>
 </template>
 
@@ -9,6 +14,17 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
+  data() {
+    return {
+      navIndex:1
+    }
+  },
+  methods: {
+    handleClick(item){
+       this.navIndex = item
+       console.log(item);
+    }
+  },
   name: 'app',
   components: {
     HelloWorld
@@ -16,13 +32,35 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+@import "./common/stylus/mixins.styl"
+$color = pink
+btnStyle(w, h, bg)
+    width w
+    height h
+    background bg
+    border none
+add(a,b)
+   a+b
+ #app 
+  color $color
+  .btn1
+    btnStyle(100px, 100px, red)
+    padding add(10px,20)
+    box-sizing content-box
+  .btn2
+    btnStyle(100px, 100px, green)
+    margin-left 10px
+  ul
+    li
+      font-size 20px
+      line-height 40px
+      bottom-border-1px($color)
+      &.active
+          color $yellow 
+    
+  
+   
+  
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+   el:'#app',
+   components: {App},
+   template: '<App />'
+})
